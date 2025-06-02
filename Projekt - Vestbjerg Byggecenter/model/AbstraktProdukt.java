5b3d5af1a359627ba68a5a0be495f32b5e99f6f2
jonatxan <jondenlange@icloud.com>
2025-06-02 11:51:21 +0200

    g

--- Projekt - Vestbjerg Byggecenter/model/AbstraktProdukt.java
+++ Projekt - Vestbjerg Byggecenter/model/AbstraktProdukt.java
@@ -1,35 +1,18 @@
 package model;
+import java.util.*;
 
+public abstract class AbstraktProdukt {
+    private String navn;
+    private double pris;
 
-/**
- * Lav en beskrivelse af klassen AbstraktProdukt her.
- * 
- * @author (dit navn her)
- * @version (versions nummer eller dato her)
- */
-public class AbstraktProdukt
-{
-    // instansvariabler - erstat eksemplet herunder med dine egne variabler
-    private int x;
-
-    /**
-     * Konstruktør for objekter af klassen AbstraktProdukt
-     */
-    public AbstraktProdukt()
-    {
-        // initialiser instansvariable
-        x = 0;
-    }
-
-    /**
-     * Et eksempel på en metode - erstat denne kommentar med din egen
-     * 
-     * @param  y  eksempel på en parameter til en metode
-     * @return    summen af x og y 
-     */
-    public int sampleMethod(int y)
-    {
-        // indsæt din egen kode her
-        return x + y;
-    }
+public AbstraktProdukt (String navn, double pris) {
+    this.navn = navn;
+    this.pris = pris;
+}
+public String getnavn() {
+    return navn;
+}
+public double getPris() {
+    return pris;
+}
 }
