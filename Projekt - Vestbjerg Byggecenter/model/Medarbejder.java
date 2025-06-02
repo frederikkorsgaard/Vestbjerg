931ce9d4be68eb5a82eeb2a50f7941a4664c29b6
jonatxan <jondenlange@icloud.com>
2025-06-02 13:06:00 +0200

    Update Medarbejder.java

--- Projekt - Vestbjerg Byggecenter/model/Medarbejder.java
+++ Projekt - Vestbjerg Byggecenter/model/Medarbejder.java
@@ -1,35 +1,18 @@
 package model;
 
-
-/**
- * Lav en beskrivelse af klassen Medarbejder her.
- * 
- * @author (dit navn her)
- * @version (versions nummer eller dato her)
- */
-public class Medarbejder
-{
-    // instansvariabler - erstat eksemplet herunder med dine egne variabler
-    private int x;
-
-    /**
-     * Konstruktør for objekter af klassen Medarbejder
-     */
-    public Medarbejder()
-    {
-        // initialiser instansvariable
-        x = 0;
-    }
-
-    /**
-     * Et eksempel på en metode - erstat denne kommentar med din egen
-     * 
-     * @param  y  eksempel på en parameter til en metode
-     * @return    summen af x og y 
-     */
-    public int sampleMethod(int y)
-    {
-        // indsæt din egen kode her
-        return x + y;
-    }
+class Medarbejder extends Person {
+    private int medarbejderID;
+    private String rolle;
+    
+public Medarbejder(int medarbejderID, String rolle, String fornavn, String efternavn, String email, int tlfnr, String adresse) {
+    super(fornavn, efternavn, email, tlfnr, adresse);
+    this.medarbejderID = medarbejderID;
+    this.rolle = rolle;
+}
+public int getID() {
+    return medarbejderID;
+}
+public String getRolle() {
+    return rolle;
+}
 }
