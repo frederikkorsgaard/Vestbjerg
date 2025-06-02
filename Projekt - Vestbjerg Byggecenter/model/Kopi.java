77a11a78fbe689bee4aa93d7e9eb71a64a4b9379
jonatxan <jondenlange@icloud.com>
2025-06-02 12:18:29 +0200

    g

--- Projekt - Vestbjerg Byggecenter/model/Kopi.java
+++ Projekt - Vestbjerg Byggecenter/model/Kopi.java
@@ -1,35 +1,10 @@
 package model;
 
-
-/**
- * Lav en beskrivelse af klassen Kopi her.
- * 
- * @author (dit navn her)
- * @version (versions nummer eller dato her)
- */
-public class Kopi
-{
-    // instansvariabler - erstat eksemplet herunder med dine egne variabler
-    private int x;
-
-    /**
-     * Konstruktør for objekter af klassen Kopi
-     */
-    public Kopi()
-    {
-        // initialiser instansvariable
-        x = 0;
-    }
-
-    /**
-     * Et eksempel på en metode - erstat denne kommentar med din egen
-     * 
-     * @param  y  eksempel på en parameter til en metode
-     * @return    summen af x og y 
-     */
-    public int sampleMethod(int y)
-    {
-        // indsæt din egen kode her
-        return x + y;
+class Kopi extends UnikProdukt {
+    private String produktNavn;
+    private String tilstand;
+    
+    public Kopi(String navn, double pris, int stelnummer, String status,String produktNavn, String tilstand) {
+        
     }
 }
