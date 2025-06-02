e4e8091563e61bc5abe5bdb0790d0cb8ab0b3dd5
jonatxan <jondenlange@icloud.com>
2025-06-02 12:47:24 +0200

    Update Person.java

--- Projekt - Vestbjerg Byggecenter/model/Person.java
+++ Projekt - Vestbjerg Byggecenter/model/Person.java
@@ -7,12 +7,17 @@ class Person {
     private String email;
     private int tlfnr;
     private String adresse;
-    
 public Person(String fornavn, String efternavn, String email, int tlfnr, String adresse) {
 this.fornavn = fornavn;
 this.efternavn = efternavn;
 this.email = email;
 this.tlfnr = tlfnr;
 this.adresse = adresse;
+}
+public String getNavn() {
+    return fornavn + " " + efternavn;
+}
+public int getTlfnr() {
+    return tlfnr;
 }
 }
