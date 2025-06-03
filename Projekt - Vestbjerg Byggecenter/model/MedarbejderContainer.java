40dcfa3ea1352eb518e9ad29580b9d59adb296b0
jonatxan <jondenlange@icloud.com>
2025-06-03 11:34:39 +0200

    gg

--- Projekt - Vestbjerg Byggecenter/model/MedarbejderContainer.java
+++ Projekt - Vestbjerg Byggecenter/model/MedarbejderContainer.java
@@ -6,14 +6,21 @@ public class MedarbejderContainer {
     private static MedarbejderContainer instance;
     private ArrayList<Medarbejder> medarbejdere;
 
+    private MedarbejderContainer() {
+        medarbejdere = new ArrayList<>();
+    }
     public static MedarbejderContainer getInstance() {
         if (instance == null) {
             instance = new MedarbejderContainer();
         }
         return instance;
     }
+    
+    public void tilføjMedarbejder(Medarbejder m) {
+        medarbejdere.add(m);
+    }
 
-    public Medarbejder getID(int id) {
+    public Medarbejder findMedarbejder(int id) {
         return null; // implementering mangler
     }
 }
