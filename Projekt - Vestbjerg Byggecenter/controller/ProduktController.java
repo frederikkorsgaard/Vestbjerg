ea6a14ebf3ed4035b5eaf6b3ff100c962dc89f7d
frederikkorsgaard <frederikkorsgaard1998@gmail.com>
2025-06-04 12:59:37 +0200

    produktcontroller

--- Projekt - Vestbjerg Byggecenter/controller/ProduktController.java
+++ Projekt - Vestbjerg Byggecenter/controller/ProduktController.java
@@ -10,18 +10,15 @@ public class ProduktController {
         simpelProduktContainer = SimpelProduktContainer.getInstance();
     }
 
-    public Produkt findProdukt(String navn) {
-        // First try to find simple product
+    public AbstraktProdukt findProdukt(String navn) {
         SimpelProdukt simpelProdukt = simpelProduktContainer.findProdukt(navn);
         if (simpelProdukt != null) {
             return simpelProdukt;
         }
         
-        // If not found, try unique product (return as Kopi)
         Kopi kopi = unikProduktContainer.findKopi(navn);
         if (kopi != null) {
-            // Create a wrapper or handle unique products differently
-            return new SimpelProdukt(kopi.getNavn(), 0.0, 1); // Placeholder
+            return kopi;
         }
         
         return null;
